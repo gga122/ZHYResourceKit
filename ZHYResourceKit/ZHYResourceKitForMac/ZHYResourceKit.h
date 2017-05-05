06cded3ece09337bf5bd8b7f16d2dfbe42577501
MickyZhu <mickyzhu@tencent.com>
2017-05-05 16:40:06 +0800

    [add] 初始化工程

---
+++ ZHYResourceKit/ZHYResourceKitForMac/ZHYResourceKit.h
@@ -0,0 +1,19 @@
+//
+//  ZHYResourceKitForMac.h
+//  ZHYResourceKit
+//
+//  Created by MickyZhu on 5/5/2017.
+//  Copyright © 2017 John Henry. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ZHYResourceKit.
+FOUNDATION_EXPORT double ZHYResourceKitVersionNumber;
+
+//! Project version string for ZHYResourceKit.
+FOUNDATION_EXPORT const unsigned char ZHYResourceKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZHYResourceKit/PublicHeader.h>
+
+
