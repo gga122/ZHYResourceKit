92abc410c8697dd97a2b29803f348007df9e6893
MickyZhu <mickyzhu@tencent.com>
2017-05-09 10:57:52 +0800

    [improve] 增加colorwrapper的category

---
+++ ZHYResourceKit/ZHYResourceKit/ZHYColorWrapper+Plist.h
@@ -0,0 +1,15 @@
+//
+//  ZHYColorWrapper+Plist.h
+//  ZHYResourceKit
+//
+//  Created by MickyZhu on 9/5/2017.
+//  Copyright © 2017 John Henry. All rights reserved.
+//
+
+#import "ZHYColorWrapper.h"
+
+@interface ZHYColorWrapper (Plist)
+
+- (instancetype)initWithPlist:(NSDictionary<NSString *, NSString *> *)plist;
+
+@end
