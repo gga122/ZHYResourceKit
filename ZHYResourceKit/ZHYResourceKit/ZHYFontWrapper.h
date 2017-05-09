96cd29c00f7d82062487b242d99246c15e967384
MickyZhu <mickyzhu@tencent.com>
2017-05-09 15:58:39 +0800

    [improve] 字体模块类完成

--- ZHYResourceKit/ZHYResourceKit/ZHYFontWrapper.h
+++ ZHYResourceKit/ZHYResourceKit/ZHYFontWrapper.h
@@ -6,8 +6,21 @@
 //  Copyright © 2017 John Henry. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#if TARGET_OS_IOS
+#import <UIKit/UIKit.h>
+typedef UIFont ZHYFont;
+#else
+#import <Cocoa/Cocoa.h>
+typedef NSFont ZHYFont;
+#endif
 
 @interface ZHYFontWrapper : NSObject
+
+- (instancetype)initWithFont:(ZHYFont *)font forName:(NSString *)name detail:(NSString *)detail NS_DESIGNATED_INITIALIZER;
+- (instancetype)init NS_UNAVAILABLE;
+
+@property (nonatomic, copy, readonly) ZHYFont *font;
+@property (nonatomic, copy, readonly) NSString name;
+@property (nonatomic, copy, readonly) NSString *detail;
 
 @end
--- ZHYResourceKit/ZHYResourceKit/ZHYFontWrapper.m
+++ ZHYResourceKit/ZHYResourceKit/ZHYFontWrapper.m
@@ -10,4 +10,38 @@
 
 @implementation ZHYFontWrapper
 
+- (instancetype)initWithFont:(ZHYFont *)font forName:(NSString *)name detail:(NSString *)detail {
+    if (!font) {
+        ZHYLogError(@"font can not be nil.");
+        return nil;
+    }
+    
+    if (!name) {
+        ZHYLogError(@"name can not be nil.");
+    }
+    
+    self = [super init];
+    if (self) {
+        _font = [font copy];
+        _name = [name copy];
+        _detail = [detail copy];
+    }
+    
+    return self;
+}
+
+#pragma mark - Overridden
+
+- (NSString *)description {
+    NSMutableString *desc = [NSMutableString stringWithString:[super description]];
+    
+    [desc appendFormat:@"<name: %@>", _name];
+    [desc appendFormat:@"<font: %@>", _font];
+    if (_detail) {
+        [desc appendFormat:@"<detail: %@>", _detail];
+    }
+    
+    return desc;
+}
+
 @end
