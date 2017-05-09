b9a80cf611286ae7fff39e584e49b93835f13f5a
MickyZhu <mickyzhu@tencent.com>
2017-05-09 11:44:40 +0800

    [improve] 增加uicolor的category

---
+++ ZHYResourceKit/ZHYResourceKitForiOS/UIColor+Hex.h
@@ -0,0 +1,15 @@
+//
+//  UIColor+Hex.h
+//  ZHYResourceKit
+//
+//  Created by MickyZhu on 9/5/2017.
+//  Copyright © 2017 John Henry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (Hex)
+
++ (UIColor *)colorWithHexARGB:(NSString *)hex;
+
+@end
---
+++ ZHYResourceKit/ZHYResourceKitForiOS/UIColor+Hex.m
@@ -0,0 +1,13 @@
+//
+//  UIColor+Hex.m
+//  ZHYResourceKit
+//
+//  Created by MickyZhu on 9/5/2017.
+//  Copyright © 2017 John Henry. All rights reserved.
+//
+
+#import "UIColor+Hex.h"
+
+@implementation UIColor (Hex)
+
+@end
