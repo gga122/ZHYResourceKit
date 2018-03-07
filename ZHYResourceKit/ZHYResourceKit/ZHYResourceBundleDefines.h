7b0cccf005a1e6f32e2dd15232d61a33557c68e3
MickyZhu <mickyzhu@tencent.com>
2018-03-07 17:40:52 +0800

    [add] 增加error头文件 [improve] 增加bundleinfo是否非法的判断接口

--- ZHYResourceKit/ZHYResourceKit/ZHYResourceBundleDefines.h
+++ ZHYResourceKit/ZHYResourceKit/ZHYResourceBundleDefines.h
@@ -9,10 +9,16 @@
 #ifndef ZHYResourceBundleDefines_h
 #define ZHYResourceBundleDefines_h
 
+#import "ZHYResourceKitDefines.h"
+#import "ZHYResourceKitErrors.h"
+
 typedef NSString * ZHYResourceBundleInfoKey;
 
+static ZHYResourceBundleInfoKey const kZHYResourceBundleMagic = @"zhy.resource.bundle.magic";
 static ZHYResourceBundleInfoKey const kZHYResourceBundleName = @"zhy.resourcekit.bundle.name";
 static ZHYResourceBundleInfoKey const kZHYResourceBundlePriority = @"zhy.resourcekit.bundle.priority";
+
+static NSString * const kZHYResourceBundleMagicValue = @"D473E2BB-79EC-43C9-919A-59A78BC32F24";
 
 NSMutableDictionary<ZHYResourceBundleInfoKey, id> *createResourceBundleTempleteInfo(void) {
     NSMutableDictionary<ZHYResourceBundleInfoKey, id> *templeteInfo = [NSMutableDictionary dictionary];
@@ -21,6 +27,24 @@ NSMutableDictionary<ZHYResourceBundleInfoKey, id> *createResourceBundleTempleteI
     [templeteInfo setObject:@(0) forKey:kZHYResourceBundlePriority];
     
     return templeteInfo;
+}
+
+
+
+BOOL isValidResourceBundleInfo(NSMutableDictionary<ZHYResourceBundleInfoKey, id> *info, NSError **error) {
+    if (info == nil || info.count == 0) {
+        return NO;
+    }
+    
+    id magic = [info objectForKey:kZHYResourceBundleMagic];
+    if (![magic isKindOfClass:[NSString class]]) {
+        ZHY_RESOURCE_KIT_ERROR(*error, kZHYResourceKitErrorDomain, ZHYResourceKitErrorCodeInvalidBundleInfo, nil);
+        return NO;
+    }
+    
+    // TODO: other values
+    
+    return YES;
 }
 
 #endif /* ZHYResourceBundleDefines_h */
---
+++ ZHYResourceKit/ZHYResourceKit/ZHYResourceKitErrors.h
@@ -0,0 +1,33 @@
+//
+//  ZHYResourceKitErrors.h
+//  ZHYResourceKit
+//
+//  Created by MickyZhu on 7/3/2018.
+//  Copyright © 2018 John Henry. All rights reserved.
+//
+
+#ifndef ZHYResourceKitErrors_h
+#define ZHYResourceKitErrors_h
+
+#define ZHY_RESOURCE_KIT_ERROR(m_error, m_domain, m_code, m_userInfo)\
+if (m_error) {\
+m_error = [NSError errorWithDomain:m_domain code:m_code userInfo:m_userInfo];\
+}\
+
+static NSErrorDomain const kZHYResourceKitErrorDomain = @"ZHYResourceKitError";
+
+/* Bundle Info */
+static NSUInteger const kZHYResourceKitErrorBundleInfoMask = 10000;
+static NSString * const kZHYResourceKitErrorBundleInfoKey = @"ZHYResourceBundleInfoKey";
+
+typedef NS_ENUM(NSInteger, ZHYResourceKitErrorCode) {
+    ZHYResourceKitErrorCodeNone                                 = 0,
+    
+    ZHYResourceKitErrorCodeInvalidBundleInfo                    = kZHYResourceKitErrorBundleInfoMask | 1,
+    
+};
+
+
+
+
+#endif /* ZHYResourceKitErrors_h */
