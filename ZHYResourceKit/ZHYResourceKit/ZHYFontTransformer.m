14a9432e4b199f41753800af7f9ee1c0d3ef480c
MickyZhu <mickyzhu@tencent.com>
2018-03-23 17:03:06 +0800

    [improve] 修改fonttransformer的实现

--- ZHYResourceKit/ZHYResourceKit/ZHYFontTransformer.m
+++ ZHYResourceKit/ZHYResourceKit/ZHYFontTransformer.m
@@ -16,6 +16,9 @@
 
 NSValueTransformerName const kZHYFontTransformer = @"zhy.resourceKit.transformer.font";
 
+static NSString * const kZHYFontInfoKeySize = @"fontSize";
+static NSString * const kZHYFontInfoKeyAttributes = @"fontAttributes";
+
 @implementation ZHYFontTransformer
 
 + (void)initialize {
@@ -34,12 +37,33 @@ NSValueTransformerName const kZHYFontTransformer = @"zhy.resourceKit.transformer
 }
 
 - (nullable ZHYFont *)transformedValue:(nullable NSDictionary *)info {
-    ZHYFont *font = [ZHYFont fontWithAttributes:info];
+    ZHYFont *font = nil;
+
+#if TARGET_OS_IOS
+    
+#else
+    CGFloat fontSize = [[info objectForKey:kZHYFontInfoKeySize] doubleValue];
+    NSDictionary *fontAttributes = [info objectForKey:kZHYFontInfoKeyAttributes];
+    NSFontDescriptor *fontDescriptor = [NSFontDescriptor fontDescriptorWithFontAttributes:fontAttributes];
+    
+    font = [NSFont fontWithDescriptor:fontDescriptor size:fontSize];
+#endif
     return font;
 }
 
 - (nullable NSDictionary *)reverseTransformedValue:(nullable ZHYFont *)font {
-    return font.attributes;
+    NSDictionary *d = nil;
+
+#if TARGET_OS_IOS
+    
+#else
+    NSDictionary *fontAttributes = font.fontDescriptor.fontAttributes;
+    CGFloat fontSize = font.pointSize;
+    
+    d = @{kZHYFontInfoKeySize: @(fontSize),
+          kZHYFontInfoKeyAttributes: fontAttributes};
+#endif
+    return d;
 }
 
 @end
